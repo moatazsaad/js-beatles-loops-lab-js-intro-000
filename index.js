38f7f4c62b213f18aaecc44b1b987656a3ee8d0d
moataz osama <moataz.osama@hotmail.com>
2020-04-01 04:30:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// add solution here
+function theBeatlesPlay(arrofmusic,arrofinst){
+  var array=[];
+  for (let i = 0 ; i < arrofmusic.length ;i++){
+
+array.push(`${arrofmusic[i]} plays ${arrofinst[i]}`)
+
+  }
+  return array
+}
