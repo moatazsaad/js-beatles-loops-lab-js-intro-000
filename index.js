d030560f683dce58850c1927f852e7314197d052
moataz osama <moataz.osama@hotmail.com>
2020-04-01 04:31:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function theBeatlesPlay(arrofmusic,arrofinst){
   var array=[];
+  var arrayrev=[];
+
   for (let i = 0 ; i < arrofmusic.length ;i++){
 
 array.push(`${arrofmusic[i]} plays ${arrofinst[i]}`)
+arrayrev.push(`$array(i)!!!`)
 
   }
-  return array
+  return array.rev
 }
